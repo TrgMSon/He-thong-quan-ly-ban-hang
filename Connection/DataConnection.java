c9a7296053326326254c02ca04a935268bb5fcd7
Trương Minh Sơn <163323332+TrgMSon@users.noreply.github.com>
2025-09-18 08:45:18 +0700

    Add files via upload

--- Connection/DataConnection.java
+++ Connection/DataConnection.java
@@ -1,1 +1,21 @@
+package Connection;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataConnection {
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/database?user=root";
+    private static final String user = "root";
+    private static final String passWord = "020677MSon@";
+
+    public static Connection setConnect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, passWord);
+        } catch(SQLException e) {
+            System.out.println("Error connect");
+        }
+        return conn;
+    }
+}
